b7587daaf7797a0de14c0951c0f02c2663551e9a
alexa <alexa@ALEXANDRAS-LAPT>
2024-04-14 23:50:17 -0500

    Add project files.

---
+++ LCP0415/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace b // folder b
+{
+    public class Order
+    {// use it outside this {}
+       public int ncogs;// private vs public 80/each
+       public int ngears;// 200/each
+       public double subtotal;
+
+       public double GetSubtotal()
+        {
+            double s = 80 * ncogs + 200 * ngears;
+            subtotal = s;
+            return s;
+        }
+
+        public string GetOrderInfor()
+        {
+            return $"N cogs: {ncogs}, N gears: {ngears}, Subtotal: {subtotal:C2}";
+        }
+    }
+}
---
+++ LCP0415/Program.cs
@@ -0,0 +1,34 @@
+﻿// MIS 3013
+// April 15, 2024
+// Alexandra Camarena
+// 113568154
+using b;// because order is in the b folder
+// list
+Console.WriteLine("List");
+
+// define an order
+// namespace b
+// the cs file name does not matter
+
+Order or1;
+or1 = new Order();
+or1.ncogs = 1;
+or1.ngears = 2; 
+double s = or1.GetSubtotal();
+string str1 = or1.GetOrderInfor();
+Console.WriteLine(str1);
+
+// order list
+Order order1;
+Order order2;
+Order order3;
+Order order4;
+
+List<Order> orderlist;// warehouse, to store Order variables, List complex, Order list
+orderlist = new List<Order>();// we build a warehouse we store the address on a card
+
+orderlist.Add(or1);//two steps;
+// 1. Create an order variable inside the order
+// 2. orderList[0] = or1;
+
+// list vs array: array fixed size warehouse. List: the size is flexible
